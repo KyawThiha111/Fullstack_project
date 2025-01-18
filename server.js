059cd608f6c7eb3d78c68eae23cf2661b5784cef
KyawThiha111 <143880386+KyawThiha111@users.noreply.github.com>
2025-01-18 07:41:42 -0800

    Raw Nodejs Practice
    In this repository, you can learn how to create a server with nodejs.

--- server.js
+++ server.js
@@ -1,2 +1,19 @@
-const command = process.argv[3];
-console.log(command)
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+const PORT = 3000;
+const server = http.createServer((req,res)=>{
+    let filePath = req.url==="/"? path.join(__dirname,"Pages","main.html"):path.join(__dirname,"Pages","profile.html")
+    
+    fs.readFile(filePath,(err,data)=>{
+        if(err) throw err;
+        res.writeHead(200, {"Content-Type": "text/html"});
+        res.end(data,"utf-8")
+    })
+
+    res.writeHead
+})
+
+server.listen(PORT,(res)=>{
+    console.log(`The sever is listening to PORT ${PORT}`);
+})
